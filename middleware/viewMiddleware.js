bf00fc190e3340797b133b1825f3aa0fd01acb8f
RstK <208619198+bowman025@users.noreply.github.com>
2026-04-11 15:21:21 +0200

    Add helped function to format file size

--- middleware/viewMiddleware.js
+++ middleware/viewMiddleware.js
@@ -4,7 +4,12 @@ const setViewLocals = (req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.formatExactDate = (date) => {
     return format(new Date(date), "PPP 'at' p");
-  }
+  };
+  res.locals.formatBytes = (bytes) => {
+    if (bytes < 1024) return `${bytes} B`;
+    if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
+    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  };
 
   next();
 }
